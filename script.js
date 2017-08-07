17cec80712914dbf24f929bffff63ab70588685c
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2017-08-08 00:17:07 +1000

    Initialized

---
+++ script.js
@@ -0,0 +1,5 @@
+var link = document.createElement("a");
+link.id = "redirecter";
+link.href = "https://www.google.com.au/search?site=&source=hp&q=how+to+avoid+facebook+and+study";
+document.getElementsByTagName("body")[0].appendChild(link);
+document.getElementById("redirecter").click();
